0459504574ef5e884b024e295759511bc86a5cb6
backurdi <bachir.kurdi@gmail.com>
2024-12-11 18:33:46 +0100

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,10 +4,21 @@ import { GrLinkedinOption } from 'react-icons/gr';
 import { HiOutlineDocumentText } from 'react-icons/hi';
 
 import { Link } from './link';
+import { TbFile } from 'react-icons/tb';
 
 export const Navigation = () => {
   return (
     <div className="flex items-center space-x-2 text-base font-semibold leading-none text-rose-100/90 sm:space-x-5">
+      <Link
+        href="/cv.pdf"
+        download
+        size="sm"
+        leftIcon={<TbFile />}
+      >
+        <span className="transition-all duration-300 ease-out group-hover:text-rose-200">
+          CV
+        </span>
+      </Link>
       <Link
         href="/about"
         leftIcon={<HiOutlineDocumentText />}
