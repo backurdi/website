859af7256a00e7f29fd85adb426746f35cc3c478
backurdi <bachir.kurdi@gmail.com>
2024-12-12 08:12:42 +0100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -34,27 +34,28 @@ const AboutPage = () => {
       <div>
         <p className="mb-5">
           My interests are many; I love to keep my mind and body
-          active. I'm a big fan of fitness, doing BJJ (Brazilian
-          Jiu Jitsu), playing Padel tennis, Bouldering, and
-          whatever can get the heart pumping. At the same time, I
-          love to read, watch movies, and play games. I also love
-          to travel and explore new places.
+          active. I&apos;m a big fan of fitness, doing BJJ
+          (Brazilian Jiu Jitsu), playing Padel tennis,
+          Bouldering, and whatever can get the heart pumping. At
+          the same time, I love to read, watch movies, and play
+          games. I also love to travel and explore new places.
         </p>
 
         <p className="mb-5">
           This combination of interests is what keeps me
           interested in always learning something and creating
-          new projects. It's really awesome what we can do these
-          days with the internet and the tools we have available.
+          new projects. It&apos;s really awesome what we can do
+          these days with the internet and the tools we have
+          available.
         </p>
 
         <p className="mb-5">
-          It's been over 7 years since I discovered the world of
-          programming. It's a discovery that never stops
-          changing. I have become a big fan of TypeScript and
-          Next.js. I have started to experiment a lot with
-          backend code with Node, and I'm also playing around
-          with Python.
+          It&apos;s been over 7 years since I discovered the
+          world of programming. It&apos;s a discovery that never
+          stops changing. I have become a big fan of TypeScript
+          and Next.js. I have started to experiment a lot with
+          backend code with Node, and I&apos;m also playing
+          around with Python.
         </p>
       </div>
     </section>
